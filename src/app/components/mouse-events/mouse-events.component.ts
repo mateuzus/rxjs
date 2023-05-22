644bfacb25ef20202877e3c3b28110325cb82c8c
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2023-05-22 20:26:55 -0300

    mouse event, the red ball follows the cursor

--- src/app/components/mouse-events/mouse-events.component.ts
+++ src/app/components/mouse-events/mouse-events.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { delay, filter, fromEvent, map } from 'rxjs';
+
+interface mouseTrack {
+  x: number
+  y: number
+}
 
 @Component({
   selector: 'app-mouse-events',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MouseEventsComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  source = fromEvent(document, 'mousemove').pipe(
+    map((e: any) => {
+      return { x: e.clientX, y: e.clientY }
+    }),
+    filter((value: mouseTrack) => value.x < 500)
+  )
+
+  onNext(value: mouseTrack) {
+    let circle: any = document.getElementById('circle')
+    circle.style.left = `${value.x}px`
+    circle.style.top = `${value.y}px`
+  }
+
+  mouseMove() {
+    this.source.subscribe({
+      next: (value) => this.onNext(value),
+      error: (e: Error) => console.log(e),
+      complete: () => console.log()
+    })
   }
 
 }
