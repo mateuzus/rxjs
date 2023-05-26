cd7f939df8e9218d06b63d058cd22a7dde20fc1a
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2023-05-26 14:40:38 -0300

    xmlHttpRequest

--- src/app/components/mouse-events/mouse-events.component.ts
+++ src/app/components/mouse-events/mouse-events.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { delay, filter, fromEvent, map } from 'rxjs';
+import { fromEvent, map } from 'rxjs';
 
 interface mouseTrack {
   x: number
   y: number
+}
+
+interface IMovie {
+  title: string
 }
 
 @Component({
@@ -13,13 +17,20 @@ interface mouseTrack {
 })
 export class MouseEventsComponent implements OnInit {
 
-  ngOnInit(): void { }
+  button: any = document.getElementById('button')
+  output: any = document.getElementById('output')
+
+  click: any = fromEvent(this.button, 'click')
+
+  ngOnInit(): void { 
+    setTimeout(() => console.log(this.click), 1000)
+  }
 
   source = fromEvent(document, 'mousemove').pipe(
     map((e: any) => {
       return { x: e.clientX, y: e.clientY }
-    }),
-    filter((value: mouseTrack) => value.x < 500)
+    })/* ,
+    filter((value: mouseTrack) => value.x < 500) */
   )
 
   onNext(value: mouseTrack) {
@@ -33,6 +44,31 @@ export class MouseEventsComponent implements OnInit {
       next: (value) => this.onNext(value),
       error: (e: Error) => console.log(e),
       complete: () => console.log()
+    })
+  }
+
+  load(url: string) {
+    let xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('load', () => {
+      let movies = JSON.parse(xhr.responseText)
+
+      movies.forEach((movie: IMovie) => {
+        let div = document.createElement('div')
+        div.innerText = movie.title
+        this.output.appendChild(div)
+      })
+    })
+
+    xhr.open('GET', url)
+    xhr.send()
+  }
+
+  clickButton() {
+    this.click.subscribe({
+      next: () => { this.load('../../../../movies.json') },
+      error: (e: Error) => { console.log(e) },
+      complete: () => { console.log('complete') }
     })
   }
 
